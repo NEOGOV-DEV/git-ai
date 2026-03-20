810bc97c499cad6f0b172eb8af64c6bd38453c91
Sergey Kats <skats@neogov.net>
2026-03-20 08:40:30 -0700

    Fixed msg for local test

--- install.ps1
+++ install.ps1
@@ -474,8 +474,6 @@ else {
     $downloadUrlNoExt = "https://github.com/$Repo/releases/latest/download/$binaryName"
 }
 
-Write-Host "Downloading from: $downloadUrlExe"
-
 # Install directory: %USERPROFILE%\.git-ai\bin
 $installDir = Join-Path $HOME ".git-ai\bin"
 New-Item -ItemType Directory -Force -Path $installDir | Out-Null
@@ -493,6 +491,7 @@ function Try-Download {
         $oldProgressPreference = $ProgressPreference
         $ProgressPreference = 'SilentlyContinue'
         try {
+            Write-Host "Downloading from: $Url"
             Invoke-WebRequest -Uri $Url -OutFile $tmpFile -UseBasicParsing -ErrorAction Stop
         } finally {
             $ProgressPreference = $oldProgressPreference
